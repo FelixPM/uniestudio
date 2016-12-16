c319a6999bd266ef4b7438f9a239edbb345b72a1
Felix Perez <perezmarquezfelix@gmail.com>
2016-12-15 23:28:39 -0300

    users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+
+
+  get 'signup' => 'users#new'
+  resources :users
+  get '/login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
